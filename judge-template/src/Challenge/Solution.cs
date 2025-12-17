b5cf11cddae7957eb02e8615e5ffe9790dbeb4d5
Nathaniel Poppe <poppent@mail.uc.edu>
2025-12-16 20:03:54 -0500

    Add minimal judge template with NUnit tests

---
+++ judge-template/src/Challenge/Solution.cs
@@ -0,0 +1,12 @@
+﻿namespace Challenge;
+
+public static class Solution
+{
+    public static int Max(int[] nums)
+    {
+        if (nums == null || nums.Length == 0)
+            throw new ArgumentException("Array cannot be null or empty");
+
+        return nums[0];
+    }
+}
---
+++ judge-template/tests/Challenge.Tests/SolutionUnitTests.cs
@@ -0,0 +1,13 @@
+﻿namespace Challenge.Tests;
+
+[TestFixture]
+public class SolutionUnitTests
+{
+    [Test]
+    public void Max_ReturnsMaximumElement()
+    {
+        int[] nums = { 1, 2, 5, 7, 3 };
+        int result = Solution.Max(nums);
+        Assert.That(result, Is.EqualTo(7));
+    }
+}
